8395a25105c319eb1072d9046b576dc99ceab293
jacquelinemathias <jacqueline.mathias38@gmail.com>
2025-08-19 21:42:12 -0300

    aaaaaaaaaaaaa

--- desenvolvimento-de-sistemas/clinica-api-05-08-25/src/app.js
+++ desenvolvimento-de-sistemas/clinica-api-05-08-25/src/app.js
@@ -31,4 +31,51 @@ app.get("/usuarios/:id", async (request, response) => {
     }
 })
 
+app.post("/usuarios", async(req, res)=> {
+    try {
+      const { body } = req
+      const usuario = await prismaClient.usuario.create({
+        data: {
+          nome: body.nome,
+          cargo: body.cargo,
+          email: body.email,
+          senha: body.senha
+        },
+      })
+      return res.status(201).json(usuario)
+    } catch (error) {
+      console.error(error)
+      if(error.code === "P2002"){
+        res.status(404).send("Falha ao cadastrar usuário: Email já cadastrado!")
+      }
+    }
+})
+
+app.put("/usuarios/:id", async(req, res)=>{
+    try {
+      const { body, params } = req
+      
+      await prismaClient.usuario.update({
+        where: { id: Number(params.id) },
+        data: { 
+          ...body
+         },
+      })
+      
+      const usuarioAtualizado = await prismaClient.usuario.findUnique({
+        where: {
+          id: Number(params.id)
+        }
+      })
+  
+      res.status(201).json({
+        message: "Usuário atualizado!",
+        data: usuarioAtualizado
+      })
+      
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
 app.listen(3000, ()=> console.log("Api rodando"))
