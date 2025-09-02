160947cc271549644205f98d316129293f07bf0f
jacquelinemathias <jacqueline.mathias38@gmail.com>
2025-09-01 21:44:02 -0300

    json test

---
+++ teste-de-sistemas/aula-jest-exemplos/aula-jest-exemplos/README.md
@@ -0,0 +1,15 @@
+# 📘 Aula Revisão – Fundamentos de JavaScript + Jest
+
+Este repositório contém os exemplos da aula de revisão de **JavaScript + Jest**.
+
+## 🚀 Como usar
+
+1. Instale as dependências:
+   ```bash
+   npm install
+   ```
+
+2. Rode todos os testes:
+   ```bash
+   npm test
+   ```
---
+++ teste-de-sistemas/json-server/tests/usersTest.js
@@ -0,0 +1,18 @@
+import jsonserver from "json-server"
+
+// let server;
+
+// beforeAll((done) => {
+//     const app = jsonserver.create();
+//     const routes = jsonserver.router("/db.json")
+//     const middleware = jsonserver.defaults();
+
+//     app.use(routes)
+//     app.use(middleware)
+
+//     server = app.listen(4000, done)
+// });
+
+// afterAll(() => {
+//     server.close();
+// });
